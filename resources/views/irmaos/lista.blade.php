31f3af211bfb1268410e0dcc9e07cc527a54a6f3
marcelomirandasilva <marcelo.miranda.pp@gmail.com>
2017-04-23 23:24:26 -0300

    testes de formulario
    alguns testes na view de irmaos

--- resources/views/irmaos/lista.blade.php
+++ resources/views/irmaos/lista.blade.php
@@ -45,15 +45,72 @@
 
                                 </td>
                                 @endforeach
-
                             </tbody>
-
                         </table>
                     </div>
-
                 </div>
             </div>
         </div>
+
+
+<form>
+    <div class="form-group">
+        <label for="no_irmao">Nome</label>
+        <input type="text" class="form-control" id="no_irmao" placeholder="Nome completo">
+    </div>
+
+  <div class="form-group">
+    <label for="email">Email address:</label>
+    <input type="email" class="form-control" id="email">
+  </div>
+
+
+
+  <div class="checkbox">
+    <label><input type="checkbox"> Remember me</label>
+  </div>
+  <button type="submit" class="btn btn-default">Submit</button>
+</form>
+
+
+<br><br><br>
+
+<div class="container-fluid" >
+   
+    <div class="row">
+        <div class="col-md-12">
+            <div class="panel with-nav-tabs panel-default">
+                <div class="panel-heading">
+                        <ul class="nav nav-tabs">
+                            <li class="active"><a href="#tab1default" data-toggle="tab">Default 1</a></li>
+                            <li><a href="#tab2default" data-toggle="tab">Default 2</a></li>
+                            <li><a href="#tab3default" data-toggle="tab">Default 3</a></li>
+                            <li class="dropdown">
+                                <a href="#" data-toggle="dropdown">Dropdown <span class="caret"></span></a>
+                                <ul class="dropdown-menu" role="menu">
+                                    <li><a href="#tab4default" data-toggle="tab">Default 4</a></li>
+                                    <li><a href="#tab5default" data-toggle="tab">Default 5</a></li>
+                                </ul>
+                            </li>
+                        </ul>
+                </div>
+                <div class="panel-body">
+                    <div class="tab-content">
+                        <div class="tab-pane fade in active" id="tab1default">Default 1aaaa</div>
+                        <div class="tab-pane fade" id="tab2default">Default 2</div>
+                        <div class="tab-pane fade" id="tab3default">Default 3</div>
+                        <div class="tab-pane fade" id="tab4default">Default 4</div>
+                        <div class="tab-pane fade" id="tab5default">Default 5</div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        
+    </div>
+</div>
+
+
+
 
 
 
