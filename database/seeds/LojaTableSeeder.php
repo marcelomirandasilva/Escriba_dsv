5b1385b7561351a43513b788fd1b9c4ef1069a92
marcelomirandasilva <marcelo.miranda.pp@gmail.com>
2017-04-28 16:55:08 -0300

    View irmaos

--- database/seeds/LojaTableSeeder.php
+++ database/seeds/LojaTableSeeder.php
@@ -11,11 +11,21 @@ class LojaTableSeeder extends Seeder
      */
     public function run()
     {
+        /*
         DB::table('loja')->insert([
             'co_titulo'   => 'ARLS',
             'no_loja'     => 'PIONEIROS DO PROGRESSO',
             'nu_loja'     => 1731,
             'dt_fundacao' => '01/01/1960',
+            'co_potencia' => 'GOB'
+        ]);
+        */
+
+        DB::table('loja')->insert([
+            'co_titulo'   => 'ARLS',
+            'no_loja'     => str_random(50),
+            'nu_loja'     => rand(1000, 9999),
+            'dt_fundacao' => '1111/11/11',
             'co_potencia' => 'GOB'
         ]);
     }
