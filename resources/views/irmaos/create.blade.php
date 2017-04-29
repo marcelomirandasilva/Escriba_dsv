9ee2185903a09a12763ecd1d4e840922cee44d9c
marcelomirandasilva <marcelo.miranda.pp@gmail.com>
2017-04-29 12:44:40 -0300

    view irmao

--- resources/views/irmaos/create.blade.php
+++ resources/views/irmaos/create.blade.php
@@ -16,49 +16,28 @@
     <div class=""> </div>
     <div class="clearfix"></div>
     <div class="row">
-        <div class="col-md-12 col-sm-12 col-xs-12">
-            <div class="x_panel">
+        <div class="col-md-12">
+            <div class="x_panel" style="background-color:  #EDEDED ">
                 <div class="x_title">
                     <h2>Cadastro de Irmãos</h2>
-
-
-
-
-                    <!--------------------------------------------------------------------------------------------------------
-                    -- MENU COM FERRAMENTAS NA LATERAL DIREITA DA DIV 
-
-                    <ul class="nav navbar-right panel_toolbox">
-                        <li>
-                            <a class="collapse-link"><i class="fa fa-chevron-up"></i></a>
-                        </li>
-                        <li class="dropdown">
-                            <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">
-                                <i class="fa fa-wrench"></i>
-                            </a>
-                            <ul class="dropdown-menu" role="menu">
-                                <li>
-                                    <a href="#">Settings 1</a>
-                                </li>
-                                <li>
-                                    <a href="#">Settings 2</a>
-                                </li>
-                            </ul>
-                        </li>
-                        <li>
-                            <a class="close-link"><i class="fa fa-close"></i></a>
-                        </li>
-                    </ul>
-                    ------------------------------------------------------------------------------------------------------
-                    -->
-
                 <div class="clearfix"></div>
                 </div>
                 <div class="x_content">
 
                 <form id="form_loja" method="post" action="" class="form-horizontal form-label-left" >
 
-{{-- CIM, NOME --}}
+<!-- NOME, CIM  ------------------------------------------------------------------------------------------>
                   <div class="x_panel">
+                    <label class="control-label col-md-1" for="no_irmao">Nome*</label>
+                    <div class="col-md-6 ">
+                        <input  id="no_irmao"   
+                                class="form-control col-md-6" 
+                                name="no_irmao" 
+                                placeholder="Nome completo do Irmão" 
+                                required="required" 
+                                type="text">
+                    </div>
+
                     <div class="item form-group">
                         <label class="control-label col-md-1" for="co_cim">CIM*</label>
                         <div  class="col-md-2" >
@@ -69,23 +48,12 @@
                                     required="required" 
                                     type="text">
                         </div>
-
-                        <label class="control-label col-md-1" for="no_irmao">Nome*</label>
-                        <div class="col-md-7 ">
-                            <input  id="no_irmao"   
-                                    class="form-control col-md-7" 
-                                    name="no_irmao" 
-                                    placeholder="Nome completo do Irmão" 
-                                    required="required" 
-                                    type="text">
-                        </div>
                     </div>
-{{-- FINAL DO CIM, NOME --}}
 
-{{-- NASCIMENTO, ESTADO CIVIL, ESCOLARIDADE, PROFISSÃO --}}
+<!-- NASCIMENTO, ESTADO CIVIL, CPF, ESCOLARIDADE, PROFISSÃO -------------------------------------------------------->
 
                     <div class="item form-group">
-                      <label class="control-label col-md-1 " for="dt_nascimento">Nascimento*</label>
+                      <label class="control-label col-md-1 " for="dt_nascimento">Nascim.*</label>
                       <div class="col-md-2 ">
                           <input id="dt_nascimento"   
                                   class="form-control col-md-2 datas_input" 
@@ -95,44 +63,20 @@
                                   type="date">
                       </div>
 
-                      <label class="control-label col-md-1 " for="ic_estado_civil">Est. Civil*</label>
+                      <label class="control-label col-md-2 " for="ic_estado_civil">Estado Civil*</label>
                       <div class="col-md-2 ">
                           <select   name="ic_estado_civil" 
                                     id="ic_estado_civil" 
                                     class="form-control col-md-2" 
                                     required="required"  >
-                              <option>Estado Civil</option>
-                              @foreach($estado_civil as $ic_estado_civil)
-                                  <option value="{{$ic_estado_civil}}"> {{$ic_estado_civil}} </option>    
-                              @endforeach
+                            <option value=""  selected style="color: #ccc;"> --- </option>
+                            @foreach($estado_civil as $ic_estado_civil)
+                                <option value="{{$ic_estado_civil}}"> {{$ic_estado_civil}} </option>    
+                            @endforeach
                           </select>
                       </div>
 
-                      <label class="control-label col-md-1 " for="ic_escolaridade"> Instrução* </label>
-                      <div class="col-md-2">
-                          <select   name="ic_escolaridade" 
-                                    id="ic_escolaridade" 
-                                    class="form-control col-md-2" 
-                                    required="required" >
-                            <option>Escolaridade</option>
-                            @foreach($escolaridade as $ic_escolaridade)
-                                <option value="{{$ic_escolaridade}}"> {{$ic_escolaridade}} </option>    
-                            @endforeach          
-                          </select>
-                        </div>
-                      </div>
-                      
-                      <label class="control-label col-md-1 " for="dt_nascimento">Profissão</label>
-                      <div class="col-md-2 ">
-                          <input id="de_profissao"   
-                                  class="form-control col-md-2 datas_input" 
-                                  name="de_profissao" 
-                                  placeholder="Profissão do Irmão" 
-                                  required="required" 
-                                  type="text">
-                      </div>
-                      
-                      <label class="control-label col-md-1" for="nu_cpf"> CPF </label>
+                      <label class="control-label col-md-1" for="nu_cpf"> CPF* </label>
                       <div class="col-md-2">
                           <input id="nu_cpf"   
                                   class="form-control col-md-2 cpf" 
@@ -142,23 +86,90 @@
                                   type="text">
                       </div>
                     </div>
-                  
 
-{{-- FINAL DO NASCIMENTO, ESTADO CIVIL, ESCOLARIDADE, PROFISSÃO --}}
 
-                        <label class="control-label col-md-1 " for="ic_grau"> Grau* </label>
-                            <div class="col-md-2">
-                                <select   name="ic_grau" 
-                                          id="ic_grau" 
-                                          class="form-control col-md-2" 
-                                          required="required" >
-                                  <option>Grau</option>
-                                  @foreach($grau as $ic_grau)
-                                      <option value="{{$ic_grau}}"> {{$ic_grau}} </option>    
-                                  @endforeach
-                                </select>
-                              </div>
+                    <div class="item form-group">
+                      <label class="control-label col-md-1 " for="ic_escolaridade"> Instrução* </label>
+                      <div class="col-md-4">
+                          <select   name="ic_escolaridade" 
+                                    id="ic_escolaridade" 
+                                    class="form-control col-md-4" 
+                                    required="required" >
+                            <option value=""  selected style="color: #ccc;"> --- </option>
+                            @foreach($escolaridade as $ic_escolaridade)
+                                <option value="{{$ic_escolaridade}}"> {{$ic_escolaridade}} </option>    
+                            @endforeach          
+                          </select>
                         </div>
+                      
+                        <label class="control-label col-md-2 " for="de_profissao">Profissão</label>
+                        <div class="col-md-3 ">
+                            <input id="de_profissao"   
+                                    class="form-control col-md-3 " 
+                                    name="de_profissao" 
+                                    placeholder="Profissão do Irmão" 
+                                    required="required" 
+                                    type="text">
+                        </div>
+                    </div>
+                  </div>
+<!------------------------------------------------------------------------------------------------------------>
+
+
+            <div class="col-md-12 ">
+              <div class="x_panel ">
+                <div class="x_title"> Iniciação </div>
+                
+              </div>
+            </div>
+
+            <div class="col-md-12 ">
+              <div class="x_panel ">
+                <div class="x_title"> Elevação </div>
+                
+              </div>
+            </div>
+
+            <div class="col-md-12 ">
+              <div class="x_panel ">
+                <div class="x_title"> Exaltação </div>
+                
+              </div>
+            </div>
+            <div class="col-md-12 ">
+              <div class="x_panel ">
+                <div class="x_title"> Instalação </div>
+                
+              </div>
+            </div>
+
+
+
+                  <div class="x_panel">
+
+                    <div class="item form-group">
+                        <label class="control-label col-md-1 " for="ic_grau"> Grau* </label>
+                        <div class="col-md-2">
+                            <select   name="ic_grau" 
+                                      id="ic_grau" 
+                                      class="form-control col-md-2" 
+                                      required="required" >
+                              <option>Grau</option>
+                              @foreach($grau as $ic_grau)
+                                  <option value="{{$ic_grau}}"> {{$ic_grau}} </option>    
+                              @endforeach
+                            </select>
+                        </div>
+                    </div>
+
+                    <div class="x_panel" class="col-md-1">
+                      <div class="x_title"> Iniciação </div>
+
+
+                    </div>
+
+
+                  </div>
 
 
 
