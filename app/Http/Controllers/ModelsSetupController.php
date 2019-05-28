c2eb941996e7bce286c2f196992ff2735f0524a2
marcelomirandasilva <marcelo.miranda.pp@gmail.com>
2019-05-27 21:31:39 -0300

    modulo setup

---
+++ app/Http/Controllers/ModelsSetupController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\ModelsSetup;
+use Illuminate\Http\Request;
+
+class ModelsSetupController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\ModelsSetup  $modelsSetup
+     * @return \Illuminate\Http\Response
+     */
+    public function show(ModelsSetup $modelsSetup)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\ModelsSetup  $modelsSetup
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(ModelsSetup $modelsSetup)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\ModelsSetup  $modelsSetup
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, ModelsSetup $modelsSetup)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\ModelsSetup  $modelsSetup
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(ModelsSetup $modelsSetup)
+    {
+        //
+    }
+}
