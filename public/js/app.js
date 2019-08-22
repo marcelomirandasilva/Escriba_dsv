da61b3c246b23ff8b9d403997c0877ba50a91bf6
root <root@srv.mmiranda.eti.br>
2019-08-22 13:05:00 +0000

    gitignore

--- public/js/app.js
+++ public/js/app.js
@@ -50418,7 +50418,7 @@ var content = __webpack_require__(63);
 if(typeof content === 'string') content = [[module.i, content, '']];
 if(content.locals) module.exports = content.locals;
 // add the styles to the DOM
-var update = __webpack_require__(65)("c77e7314", content, false, {});
+var update = __webpack_require__(65)("7cade823", content, false, {});
 // Hot Module Replacement
 if(false) {
  // When the styles change, update the <style> tags
